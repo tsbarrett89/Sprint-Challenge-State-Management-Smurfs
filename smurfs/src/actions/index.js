99bd7526dd7be31831f28764cfc2271e251315ed
Thomas Barrett <tsbarrett89@gmail.com>
2019-11-08 09:30:53 -0700

    started redux setup

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const FETCH_START = 'FETCH_START'
+export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+export const FETCH_FAILURE = 'FETCH_START'
+export const POST_START = 'POST_FAILURE'
+export const POST_SUCCESS = 'POST_SUCCESS'
+export const POST_FAILURE = 'POST_FAILURE'
